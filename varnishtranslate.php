e13dd0c1bc8bd89dd24d43b3709b2f38e0e8c7ce
Willem Kappers <willemk@gmail.com>
2014-01-20 16:50:42 +0000

    added Cookie handling Use case

--- varnishtranslate.php
+++ varnishtranslate.php
@@ -1,9 +1,19 @@
-<h1>Varnish Translate</h1>
+<code>
 <pre>
 
 sub devicedetect {
 	unset req.http.X-UA-Device;
 	set req.http.X-UA-Device = "browser";
+	# Handle that a cookie may override the detection alltogether.
+	if (req.http.Cookie ~ "(?i)X-UA-Device-force") {
+		/* ;?? means zero or one ;, non-greedy to match the first. */
+		set req.http.X-UA-Device = regsub(req.http.Cookie, "(?i).*X-UA-Device-force=([^;]+);??.*", "\1");
+		/* Clean up our mess in the cookie header */
+		set req.http.Cookie = regsuball(req.http.Cookie, "(^|; ) *X-UA-Device-force=[^;]+;? *", "\1");
+		/* If the cookie header is now empty, or just whitespace, unset it. */
+		if (req.http.Cookie ~ "^ *$") { unset req.http.Cookie; }
+	} else {
+
 <?php
 
 $jsonRules =  file_get_contents("https://raw.github.com/serbanghita/Mobile-Detect/master/Mobile_Detect.json");
@@ -23,16 +33,23 @@ echo returnVarnishRules($desktop,"desktop");
 
 $os = $rules->uaMatch->browsers;
 echo returnVarnishRules($phones,"os");
+?>
+	}
+}
 
+
+
+
+<?
 function returnVarnishRules($rulesArray, $key, $else = true){
-	$retString = "\t";
+	$retString = "\t\t";
 	if ($else){
 		$retString .= "els";
 	}
 	$retString .= "if (\n";
 	$count = 0;
 	foreach($rulesArray as $rule){
-		$retString .= "\t";
+		$retString .= "\t\t";
 		$retString .= "   (req.http.User-Agent ~ \"$rule\")"; 
 		if ($count < (count((array)$rulesArray) -1)){
 			$retString .= " ||\n";
@@ -41,12 +58,13 @@ function returnVarnishRules($rulesArray, $key, $else = true){
 		} 
 		$count++;
 	}
-	$retString .= "\t\tset req.http.X-UA-Device = \"$key\";\n";
-	$retString .= "\t}\n\n";
+	$retString .= "\t\t\tset req.http.X-UA-Device = \"$key\";\n";
+	$retString .= "\t\t}\n\n";
 
 	return $retString;
 
 }
 
 ?>
-
+</pre>
+</code>
