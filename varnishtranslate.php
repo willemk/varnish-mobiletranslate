3f438f9d9f095eea510052d5b8155c6b3111d451
miniyarov <miniyarov@gmail.com>
2016-11-09 10:50:43 +0300

    Updates to MobileDetect 2.8.22, Fixes tablet if -> elsif

--- varnishtranslate.php
+++ varnishtranslate.php
@@ -1,5 +1,5 @@
 <?php
-$jsonRules =  file_get_contents("https://raw.github.com/serbanghita/Mobile-Detect/master/Mobile_Detect.json");
+$jsonRules = file_get_contents("https://raw.github.com/serbanghita/Mobile-Detect/master/Mobile_Detect.json");
 
 $rules = json_decode($jsonRules);
 
@@ -8,13 +8,13 @@ $rules = json_decode($jsonRules);
 # All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or without
-# modification, are permitted provided that the following conditions are met: 
-# 
+# modification, are permitted provided that the following conditions are met:
+#
 # 1. Redistributions of source code must retain the above copyright notice, this
-#    list of conditions and the following disclaimer. 
+#    list of conditions and the following disclaimer.
 # 2. Redistributions in binary form must reproduce the above copyright notice,
 #    this list of conditions and the following disclaimer in the documentation
-#    and/or other materials provided with the distribution. 
+#    and/or other materials provided with the distribution.
 #
 # THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
 # ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
@@ -26,9 +26,9 @@ $rules = json_decode($jsonRules);
 # ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
 # (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
 # SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-# 
+#
 # The views and conclusions contained in the software and documentation are those
-# of the authors and should not be interpreted as representing official policies, 
+# of the authors and should not be interpreted as representing official policies,
 # either expressed or implied, of the FreeBSD Project
 # mobile_detect.vcl - Drop-in varnish solution to mobile user detection based on the Mobile-Detect library
 #
@@ -37,8 +37,8 @@ $rules = json_decode($jsonRules);
 # Author: Willem Kappers
 
 sub devicedetect {
-	#Based on Mobile detect <?php echo $rules->version?>
-	
+	#Based on Mobile detect <?php echo $rules->version ?>
+
 	#https://github.com/serbanghita/Mobile-Detect
 	unset req.http.X-UA-Device;
 	set req.http.X-UA-Device = "desktop";
@@ -54,57 +54,58 @@ sub devicedetect {
 
 <?php
 
-
-$phones = $rules->uaMatch->phones;
-echo returnVarnishRules($phones,"mobile");
+/**
+ * Mobile Bots visit sites alot. In order to calculate regex fast
+ * MobileBots must be first. Also it has much less expressions than Phones or Tablets
+ */
+$mobileBots = array($rules->uaMatch->utilities->MobileBot);
+echo returnVarnishRules($mobileBots, 'mobile', false);
 
 $mobileBrowsers = $rules->uaMatch->browsers;
-echo returnVarnishRules($mobileBrowsers,"mobile",false, true);
+echo returnVarnishRules($mobileBrowsers, "mobile", false, true);
 
 $mobileOS = $rules->uaMatch->os;
-echo returnVarnishRules($mobileOS,"mobile",false, true);
+echo returnVarnishRules($mobileOS, "mobile", false, true);
+
+$phones = $rules->uaMatch->phones;
+echo returnVarnishRules($phones, "mobile", false, true);
 
 $tablets = $rules->uaMatch->tablets;
-echo returnVarnishRules($tablets,"tablet",true);
-
-$mobileBots = array($rules->uaMatch->utilities->MobileBot);
-echo returnVarnishRules($mobileBots, 'mobile', false, true);
+echo returnVarnishRules($tablets, "tablet", true, true);
 
 ?>
-	}
+}
 }
 
-
-
-
 <?php
-function returnVarnishRules($rulesArray, $key, $tablet = false, $useElse = false){
-	$retString = "\t\t";
-	if ($useElse){
-		$retString .= "elsif (\n";
-	}else{
-			
-		$retString .= "if (\n";
-	}
-	$count = 0;
-	foreach($rulesArray as $rule){
-		$retString .= "\t\t";
-		$retString .= "   (req.http.User-Agent ~ \"(?i)$rule\")"; 
-		if ($count < (count((array)$rulesArray) -1)){
-			$retString .= " ||\n";
-		}else{
-			 $retString .= ") {\n";	
-		} 
-		$count++;
-	}
-	if ($tablet){
-		$retString .= "\t\t\tset req.http.X-UA-Device = \"mobile;$key\";\n";
-	}else{
-		$retString .= "\t\t\tset req.http.X-UA-Device = \"$key\";\n";
-	}
-	$retString .= "\t\t}\n\n";
+function returnVarnishRules($rulesArray, $key, $tablet = false, $useElse = false)
+{
+    $retString = "\t\t";
+    if ($useElse) {
+        $retString .= "elsif (\n";
+    } else {
 
-	return $retString;
+        $retString .= "if (\n";
+    }
+    $count = 0;
+    foreach ($rulesArray as $rule) {
+        $retString .= "\t\t";
+        $retString .= "   (req.http.User-Agent ~ \"(?i)$rule\")";
+        if ($count < (count((array)$rulesArray) - 1)) {
+            $retString .= " ||\n";
+        } else {
+            $retString .= ") {\n";
+        }
+        $count++;
+    }
+    if ($tablet) {
+        $retString .= "\t\t\tset req.http.X-UA-Device = \"mobile;$key\";\n";
+    } else {
+        $retString .= "\t\t\tset req.http.X-UA-Device = \"$key\";\n";
+    }
+    $retString .= "\t\t}\n\n";
+
+    return $retString;
 
 }
 
